f6efd2ffd67a36ac187ffcce5a68c77183c6f722
jjarzynski <jjarzynski@evojam.com>
2021-01-10 15:33:50 +0100

    Uncommented final 2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -63,12 +63,12 @@ VALUES (1, 1, 'Co ma wspólnego łyżka z jesienią?', 'Je się nią!'),
 --
 --
 
--- INSERT INTO JOKE_OWNER (id, comedian_id, joke_id, since, until)
--- VALUES (OWNER_SEQ.nextval, 1, 1, '2020-12-31', null),
---        (OWNER_SEQ.nextval, 2, 2, '2020-12-31', null),
---        (OWNER_SEQ.nextval, 3, 3, '2020-12-31', null),
---        (OWNER_SEQ.nextval, 4, 4, '2020-12-31', null)
--- ;
+INSERT INTO JOKE_OWNER (id, comedian_id, joke_id, since, until)
+VALUES (OWNER_SEQ.nextval, 1, 1, '2020-12-31', null),
+       (OWNER_SEQ.nextval, 2, 2, '2020-12-31', null),
+       (OWNER_SEQ.nextval, 3, 3, '2020-12-31', null),
+       (OWNER_SEQ.nextval, 4, 4, '2020-12-31', null)
+;
 
 --
 --
@@ -98,12 +98,12 @@ VALUES (1, 1, 'Co ma wspólnego łyżka z jesienią?', 'Je się nią!'),
 --
 --
 
--- INSERT INTO JOKE_VERSION (id, joke_id, question, since)
--- VALUES (JOKE_VERSION_SEQ.nextval, 1, 'Co ma wspólnego łyżka z jesienią?', '2020-12-31'),
---        (JOKE_VERSION_SEQ.nextval, 2, 'Co na wojnie nigdy się nie psuje?', '2020-12-31'),
---        (JOKE_VERSION_SEQ.nextval, 3, 'Co fryzjerka ma w lodówce?', '2020-12-31'),
---        (JOKE_VERSION_SEQ.nextval, 4, 'Jak się czuje magik w 2020?', '2020-12-31')
--- ;
+INSERT INTO JOKE_VERSION (id, joke_id, question, since)
+VALUES (JOKE_VERSION_SEQ.nextval, 1, 'Co ma wspólnego łyżka z jesienią?', '2020-12-31'),
+       (JOKE_VERSION_SEQ.nextval, 2, 'Co na wojnie nigdy się nie psuje?', '2020-12-31'),
+       (JOKE_VERSION_SEQ.nextval, 3, 'Co fryzjerka ma w lodówce?', '2020-12-31'),
+       (JOKE_VERSION_SEQ.nextval, 4, 'Jak się czuje magik w 2020?', '2020-12-31')
+;
 
 --
 --
